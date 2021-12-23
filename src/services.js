ee729f564604ea53aa81d985c86df8b6ffbf354e
Piero Pozo <54334640+58pier@users.noreply.github.com>
2021-12-23 18:27:10 -0500

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,12 +1,13 @@
-function reader(name) {
-  const fs = require("fs");
-  const pdf = require("pdf-parse");
-
+const fs = require("fs");
+const pdf = require("pdf-parse");
+const reader = async (name) => {
   let dataBuffer = fs.readFileSync(`${name}`);
-
-  pdf(dataBuffer).then(function (data) {
+  const datos = await pdf(dataBuffer).then(function (data) {
     // PDF text
-    return data.text;
+    const datos = data.text.trim();
+    // console.log(datos);
+    return datos;
   });
-}
+  return datos;
+};
 module.exports = reader;
